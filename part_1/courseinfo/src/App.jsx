9e6c764b4b81256fc7a2c4d2627fbc555cf386b1
andrew-luo1 <andrew.luo1@outlook.com>
2025-08-02 20:44:44 -0400

    exercise 1.1

---
+++ part_1/courseinfo/src/App.jsx
@@ -0,0 +1,53 @@
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <p>
+        {props.p1} {props.e1}
+      </p>
+      <p>
+        {props.p2} {props.e2}
+      </p>
+      <p>
+        {props.p3} {props.e3}
+      </p>
+    </div>
+
+  )
+}
+
+const Footer = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.count}</p>
+    </div>
+  )
+}
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course = {course}/>
+      <Content p1={part1} e1={exercises2}
+               p2={part2} e2={exercises2}
+               p3={part3} e3={exercises3}/>
+      <Footer count={exercises1 + exercises2 + exercises3}/>
+    </div>
+  )
+}
+
+export default App
